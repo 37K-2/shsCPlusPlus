f5f04c9ae9ff5f4533559301cb1969378532f175
37K-2 <aguo7531@gmail.com>
2022-12-16 01:54:34 -0800

    Zuul Commit 2

--- Zuul/room.cpp
+++ Zuul/room.cpp
@@ -1,5 +1,32 @@
 #include <cstring>
+#include <vector>
+#include <map>
 #include "room.h"
 
-parent::room(){}
+parent::room(){
+  items = new vector<char *>();
+  exits = new map<char *, room *>();
+  name = new char[251];
+}
+
+void addItem(char* item){
+  items->push_back(item);
+}
+
+void setExit(char* direction, room* rm){
+  if(exits->size()==0){
+    exit->insert({direction, rm});
+    return;
+  }
+  map<char*, room*>::iterator it = exits->begin();
+  while(it!=exits->end()){
+    if(strcmp(it->first, direction) == 0)
+      return;
+    else{
+      exits->insert({direction, rm});
+      return;
+    }
+  }
+}
+
 
